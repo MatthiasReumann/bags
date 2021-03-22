62d607c93dbd2a4f8f1e493177ea6422497b7d30
Matthias Reumann <reumann.matthias@student.htlwrn.ac.at>
2021-03-22 16:19:23 +0100

    Add user

---
+++ models/user.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ObjectID = mongoose.Types.ObjectId;
+
+// this is not the same schema as defined in 'item.js'
+// here a item is just an id referencing an item and the amount 
+// which is in a bag
+const itemSchema = new Schema({
+    id: ObjectID,
+    amount: {
+        type: Number,
+        min: 0,
+        default: 0
+    }
+})
+const bagSchema = new Schema({
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    name: {
+        type: String,
+        default: "my bag"
+    },
+    items: [itemSchema]
+})
+
+const userSchema = new Schema({
+    oauth_id: {
+        type: String
+    },
+    bags: [bagSchema],
+    favorites: [ObjectID]
+});
+
+module.exports = userSchema;
